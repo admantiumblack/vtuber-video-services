3bce9fb257420f0cf1800236d399399801d11d76
Roland <felixroland12355@gmail.com>
2023-03-21 15:42:53 +0700

    setup: setup run server

---
+++ Server/Config/server_config.py
@@ -0,0 +1,11 @@
+from pydantic import BaseSettings
+from functools import lru_cache
+
+class ServerSettings(BaseSettings):
+    host: str = '127.0.0.1'
+    port: int = 8000
+    reload: bool = True
+
+@lru_cache
+def get_server_setting():
+    return ServerSettings()
