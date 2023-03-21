e60ab58759282889660338ea9a8bb22f7a3d34d4
Roland <felixroland12355@gmail.com>
2023-03-21 17:45:40 +0700

    config: create youtube api configuration

---
+++ Server/Config/youtube_config.py
@@ -0,0 +1,10 @@
+from pydantic import BaseSettings
+from functools import lru_cache
+
+class YoutubeSettings(BaseSettings):
+    youtube_host: str = 'youtube.com'
+    secret_key: str = ''
+    
+@lru_cache
+def get_youtube_settings():
+    return YoutubeSettings()
