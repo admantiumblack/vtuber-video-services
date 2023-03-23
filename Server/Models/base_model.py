caf811437319bba5730041afd44e902c6f4991ee
Roland <felixroland12355@gmail.com>
2023-03-23 20:29:52 +0700

    fix: fix model to be abstract model

--- Server/Models/base_model.py
+++ Server/Models/base_model.py
@@ -4,6 +4,6 @@ from sqlalchemy import Column, DateTime
 Base = declarative_base()
 
 class DateTimeModel(Base):
-    __tablename__ = None
+    __abstract__ = True
     begin_date = Column(DateTime, primary_key=False, index=False)
     end_date = Column(DateTime, primary_key=False, index=False)
