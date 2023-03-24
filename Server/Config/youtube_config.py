d613648e311a11ef6d05bbf96d44044467e51d33
Roland <felixroland12355@gmail.com>
2023-03-24 21:10:08 +0700

    create url builder to automate url building

--- Server/Config/youtube_config.py
+++ Server/Config/youtube_config.py
@@ -1,9 +1,17 @@
+from Server.Utils.url_utils import build_url
 from pydantic import BaseSettings
 from functools import lru_cache
 
 class YoutubeSettings(BaseSettings):
     youtube_host: str = 'youtube.com'
     youtube_secret_key: str = ''
+
+    def build_url(self, path, param:dict={}):
+        param_with_key = {
+            'key': self.youtube_secret_key,
+            **param
+        }
+        return build_url(self.youtube_host+'/'+path, param_with_key)
     
 @lru_cache
 def get_youtube_settings():
