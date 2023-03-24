3ed5b4e735274b63bd5611eb43d9876bb633e177
Roland <felixroland12355@gmail.com>
2023-03-24 21:09:47 +0700

    add: add field to video schema

--- Server/Schemas/video_schema.py
+++ Server/Schemas/video_schema.py
@@ -2,3 +2,5 @@ from Server.Schemas.base_schema import YoutubeSchema
 
 class VideoSchema(YoutubeSchema):
     channel_id: str
+    parts:str = 'liveStreamingDetails,statistics,topicDetails,snippet'
+    playlist_type:str = 'uploads'
